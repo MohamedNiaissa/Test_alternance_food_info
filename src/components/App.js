dd047cf815d71b915e22ec4049def654009c3d90
Mohamed <mohameddrame907@gmail.com>
2022-07-31 17:57:25 +0200

    test stockage de données d'un produit au click

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import { render } from "@testing-library/react"
 import Header from "./Header";
 // import Main from "./Main";
 import Produits from "./Produits"
+import '../css/App.css'
 
 function App() {
   
--- src/components/Produitjs.js
+++ src/components/Produitjs.js
@@ -25,19 +25,18 @@ fetch(url, {
             generalDiv.appendChild(div);
     
             let p = document.createElement('p');
-            document.querySelector(".div"+i).appendChild(p);
             p.innerHTML += data.products[i].brands_tags[0];
-        
+            
             let image = document.createElement("img");
             image.className = "img"+i;
             image.setAttribute("src",data.products[i].image_front_small_url);
             image.style.width = "100px";
             image.style.height = "100px";
-
+            
             document.querySelector(".div"+i).appendChild(image);
-
-
-            div.style.border = "13px solid #0000";
+            document.querySelector(".div"+i).appendChild(p);
+            
+            // div.style.border = "13px solid #0000";
             div.style.borderRadius = "3px";
             div.style.boxShadow = "2px 3px 2px 2px";
             div.style.width = "200px";
@@ -48,6 +47,18 @@ fetch(url, {
             div.style.alignItems = "center";
             div.style.justifyContent = "center";
             div.style.flex = "0 1 300px";
+
+            div.style.background = "rgba( 255, 255, 255, 0.25 )";
+            div.style.boxShadow = "0 8px 32px 0 rgba( 31, 38, 135, 0.37 )";
+            div.style.border = "border: 1px solid rgba( 255, 255, 255, 0.18 )";
+
+
+            div.addEventListener('click',function(){
+                window.localStorage.setItem('categories',data.products[i].categories)
+            })
+            
+
+
         }
 
         
@@ -55,3 +66,12 @@ fetch(url, {
         
     })
 })
+
+/*
+background: rgba( 255, 255, 255, 0.25 );
+box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
+backdrop-filter: blur( 8px );
+-webkit-backdrop-filter: blur( 8px );
+border-radius: 10px;
+border: 1px solid rgba( 255, 255, 255, 0.18 );
+*/
