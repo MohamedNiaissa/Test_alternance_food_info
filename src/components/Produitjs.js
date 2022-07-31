3a28a23409c8dfb2a16fae4b6749c200726d47c6
Mohamed <mohameddrame907@gmail.com>
2022-07-31 17:11:44 +0200

    apparition des images

--- src/components/Produitjs.js
+++ src/components/Produitjs.js
@@ -2,7 +2,6 @@
 const url = "https://world.openfoodfacts.org?json=true"
 let headers = {};
 
-let r = document.querySelector('.root');
 fetch(url, {
     method : "GET",
     mode: 'cors',
@@ -10,12 +9,48 @@ fetch(url, {
 }).then((response) => {
     return response.json().then((data) => {
         console.log(data);
-        alert(data);
-        let p = document.createElement('p');
-        p.innerHTML = "data = " + data.products[0].brands;
+        // alert(data);
+        
+        let generalDiv = document.createElement('div');
 
-        document.body.appendChild(p);
+        generalDiv.style.display = "flex";
+        generalDiv.style.flexWrap = "wrap";
+        generalDiv.className = "generalDiv";
+        generalDiv.style.justifyContent = "flex-end";
+        document.body.appendChild(generalDiv);
 
+        for(let i = 0; i < data.products.length; i++){
+            let div = document.createElement('div');
+            div.className = "div"+i;
+            generalDiv.appendChild(div);
+    
+            let p = document.createElement('p');
+            document.querySelector(".div"+i).appendChild(p);
+            p.innerHTML += data.products[i].brands_tags[0];
+        
+            let image = document.createElement("img");
+            image.className = "img"+i;
+            image.setAttribute("src",data.products[i].image_front_small_url);
+            image.style.width = "100px";
+            image.style.height = "100px";
+
+            document.querySelector(".div"+i).appendChild(image);
+
+
+            div.style.border = "13px solid #0000";
+            div.style.borderRadius = "3px";
+            div.style.boxShadow = "2px 3px 2px 2px";
+            div.style.width = "200px";
+            div.style.height = "200px";
+            div.style.margin = "10px";
+            div.style.display = "flex";
+            div.style.flexDirection = "column";
+            div.style.alignItems = "center";
+            div.style.justifyContent = "center";
+            div.style.flex = "0 1 300px";
+        }
+
+        
 
         
     })
